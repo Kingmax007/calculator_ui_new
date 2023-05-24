3bf9221dcd123e45e6bfaf85793dc334a199a76c
Kingmax007 <kingsley.chukwuka.okwurionu@eek.ee>
2023-05-24 08:41:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# calculator_ui_new
+
+A new Flutter project.
+
+## Getting Started
+
+A simple calculator with functionality and a history function ;
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+
+import 'package:calculator_ui_new/model/historyitem.dart';
+import 'package:calculator_ui_new/provider/calculator_provider.dart';
+import 'package:calculator_ui_new/screens/history.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'imports.dart';
+import 'screens/calculator.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(HistoryItemAdapter());
+  await Hive.openBox<HistoryItem>('history');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<CalculatorProvider>(
+      create: (_) => CalculatorProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: backgroundColor,
+          appBarTheme: AppBarTheme(
+            color: backgroundColor,
+            elevation: 0.0,
+          ),
+          textTheme: TextTheme(
+            headline3: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            caption: TextStyle(
+              fontWeight: FontWeight.w500,
+              color: Colors.grey[700],
+              fontSize: 18.0,
+            ),
+          ),
+          colorScheme:
+          ColorScheme.fromSwatch().copyWith(secondary: redColor),
+        ),
+        routes: {
+          '/': (context) => Calculator(),
+          '/history': (context) => History(),
+        },
+      ),
+    );
+  }
+}
